80d630ad5976ca88f5b47de018c12d4ed2e81ae8
DESKTOP-OD4BAJ8\mgtpa <pokemon0116@i.softbank.jp>
2024-06-05 00:54:14 +0900

    block

---
+++ Assets/BarController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarController : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody myRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity = new Vector3(Input.GetAxis("Horizon")*speed,0,0);
+    }
+}
